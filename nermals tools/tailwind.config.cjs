ccf9e76530684c2012b43a2a33a4dc2b1b577f95
Arjun <arjunaditya80@gmail.com>
2023-08-04 17:09:15 +0530

    first commit

---
+++ nermals tools/tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+
+    extend: {
+            colors: {
+        // Add or modify the background color
+        body: '#0c1215', // Replace with your desired color code
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
